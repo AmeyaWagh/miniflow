12db6eed5b8dd1fccb287de747fee2223f741546
ameyawagh <ameya555@gmail.com>
2018-03-10 18:27:00 -0500

    linear and sigmoid added

--- MiniFlow.py
+++ MiniFlow.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+import numpy as np
 
 class Node(object):
     def __init__(self, inbound_nodes=[]):
@@ -64,9 +65,6 @@ class Add(Node):
             value+=node.value
         # self.value = reduce(lambda x,y:x.value+y.value,self.inbound_nodes)
         self.value=value
-"""
-No need to change anything below here!
-"""
 
 class Multiply(Node):
     def __init__(self, *inputs):
@@ -79,6 +77,72 @@ class Multiply(Node):
             value*=node.value
         # self.value = reduce(lambda x,y:x.value+y.value,self.inbound_nodes)
         self.value=value
+
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value to the value of the linear function output.
+
+        Your code goes here!
+        """
+        # for _input _weight in zip(iter1, iter2)
+        # for node in self.inbound_nodes:
+        #     print(node.value) 
+        inputs = np.array(self.inbound_nodes[0].value)
+        weights = np.array(self.inbound_nodes[1].value)
+        bias = np.array(self.inbound_nodes[2].value)
+        # self.value = 
+        # print("inputs",inputs)
+        # print("weights",weights)
+        # print("bias",bias)
+        # print("value",np.matmul(inputs,weights)+bias)
+        # self.value = np.matmul(inputs,weights)+bias
+        self.value = np.dot(inputs, weights) + bias
+
+class Sigmoid(Node):
+    """
+    You need to fix the `_sigmoid` and `forward` methods.
+    """
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+
+        Your code here!
+        """
+        # sigmoid = lambda n:(1./1.+np.exp(-1*n))
+        # meta = Linear(inputs, weights, bias)
+        # return map(sigmoid,x)
+        return 1./(1.+np.exp(-x))
+
+    def forward(self):
+        """
+        Set the value of this node to the result of the
+        sigmoid function, `_sigmoid`.
+
+        Your code here!
+        """
+        # This is a dummy value to prevent numpy errors
+        # if you test without changing this method.
+        # for node in self.inbound_nodes:
+        #     print(self._sigmoid(node.value))
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
 
 
 def topological_sort(feed_dict):
--- nn.py
+++ nn.py
@@ -2,7 +2,7 @@
 
 from MiniFlow import * 
 
-if __name__ == '__main__':
+def test_add_mul():
     x, y, z = Input(), Input(), Input()
 
     # f = Add(x, y, z)
@@ -19,3 +19,52 @@ if __name__ == '__main__':
     # should output 19
     # print("{} + {} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
     print("{} * {} * {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
+
+
+def test_linear():
+    inputs, weights, bias = Input(), Input(), Input()
+
+    f = Linear(inputs, weights, bias)
+
+    feed_dict = {
+        inputs: [6, 14, 3],
+        weights: [0.5, 0.25, 1.4],
+        bias: 2
+    }
+
+    graph = topological_sort(feed_dict)
+    output = forward_pass(f, graph)
+
+    print(output) # should be 12.7 with this example
+
+
+def test_sigmoid():
+    X, W, b = Input(), Input(), Input()
+
+    f = Linear(X, W, b)
+    g = Sigmoid(f)
+
+    X_ = np.array([[-1., -2.], [-1, -2]])
+    W_ = np.array([[2., -3], [2., -3]])
+    b_ = np.array([-3., -5])
+
+    feed_dict = {X: X_, W: W_, b: b_}
+
+    graph = topological_sort(feed_dict)
+    # for gf in graph:
+    #     print("gf >> ",gf)
+    output = forward_pass(g, graph)
+
+    """
+    Output should be:
+    [[  1.23394576e-04   9.82013790e-01]
+     [  1.23394576e-04   9.82013790e-01]]
+    """
+    print(output)
+
+
+
+if __name__ == '__main__':
+    # test_add_mul()
+    # test_linear()
+    test_sigmoid()
